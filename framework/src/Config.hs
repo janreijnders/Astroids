d916c69634bb19694c745842b0555fb8c97c87c6
janreijnders <jan@tor.nl>
2015-10-22 16:13:21 +0200

    Test commit

--- framework/src/Config.hs
+++ framework/src/Config.hs
@@ -10,3 +10,5 @@ aspectRatio, defaultVerticalResolution, defaultHorizontalResolution :: Float
 aspectRatio                 = 16/9
 defaultVerticalResolution   = defaultHorizontalResolution / aspectRatio
 defaultHorizontalResolution = 1024
+--Test
+--Test
